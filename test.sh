a495a85eafca323c4d157c436cb5e210be4979ce
z3451444 <z3451444@cse.unsw.edu.au>
2020-08-02 12:30:17 +1000

    added auto test script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+folder="examples"
+sub_folder="0"
+
+dir="$folder/$sub_folder/"
+for file in "$dir"*.sh
+do
+    file_name=`echo "$file" | sed "s/$folder\/$sub_folder\///g;s/\..*$//g"`
+    shell_file="$file_name"".sh"
+    perl_file="$file_name"".pl"
+    echo "Checking $file_name ..."
+    `./sheeple.pl "$folder/$sub_folder/$shell_file" > tmp.pl`
+    if ! diff tmp.pl $folder/$sub_folder/$perl_file >/dev/null
+    then
+        echo "!!different!!"
+    fi
+done
